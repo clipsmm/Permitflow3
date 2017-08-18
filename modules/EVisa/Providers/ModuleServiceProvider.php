755d1dde1c5da8bb8530a58729f51617c3c35e16
Steve Ops <steve.opiyo5@gmail.com>
2017-08-18 18:37:31 +0300

    Install and configure caffeinated modules

---
+++ modules/EVisa/Providers/ModuleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\EVisa\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'e-visa');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'e-visa');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations', 'e-visa');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
