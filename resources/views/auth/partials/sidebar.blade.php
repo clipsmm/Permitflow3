1cfe0c10f2baeff5e3b0b2499c2e600e6a287767
DESKTOP-7FKJIS9\sonfo <sonfordonyango@gmail.com>
2017-08-18 19:12:13 +0300

    added side bar photo

--- resources/views/auth/partials/sidebar.blade.php
+++ resources/views/auth/partials/sidebar.blade.php
@@ -2,16 +2,12 @@
 
 @section('content')
     <div class="container">
+
         <div class="row">
 
             <div class="col-md-3 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="thumbnail thumbnail-reveal">
-                            <img class="img-responsive hidden-xs" src="https://eresident.co.ke/uploads/20170201175906.png">
-                            <div class="reveal-holder"><a class="btn btn-default" href="#avatar-modal" data-toggle="modal" role="button"><i class="fa fa-camera"></i> Change Photo</a></div>
-                        </div>
-                    </div>
+                    <div class="panel-heading">Dashboard</div>
 
                     <div class="panel-body">
                         Evisa
@@ -24,19 +20,15 @@
                 </div>
             </div>
 
-
             <div class="col-md-8">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Frontend Dashboard</div>
+                    <div class="panel-heading">Dashboard</div>
 
                     <div class="panel-body">
                         You are logged in!
                     </div>
                 </div>
             </div>
+        </div>
     </div>
-    </div>
-
-
-
 @endsection
--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -3,15 +3,35 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+
+            <div class="col-md-3 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Backend Dashboard</div>
+                    <div class="panel-heading">
+                            <div class="thumbnail thumbnail-reveal">
+                                <img class="img-responsive hidden-xs" src="https://eresident.co.ke/uploads/20170201175906.png">
+                                <div class="reveal-holder"><a class="btn btn-default" href="#avatar-modal" data-toggle="modal" role="button"><i class="fa fa-camera"></i> Change Photo</a></div>
+                            </div>
+                    </div>
 
                     <div class="panel-body">
-                        Backend content goes here!
+                        Evisa
+                    </div>
+
+                    <div class="panel-body">
+                        E-Passport
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+
+                    <div class="panel-body">
+                        You are logged in!
                     </div>
                 </div>
             </div>
-        </div>
     </div>
 @endsection
