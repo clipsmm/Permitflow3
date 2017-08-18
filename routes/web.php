12656284409d889796f5cc28dfea5c9d309fe2f7
DESKTOP-7FKJIS9\sonfo <sonfordonyango@gmail.com>
2017-08-18 18:33:59 +0300

    frontend and dashboard views

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/frontend', 'FrontendController@index')->name('frontend');
+Route::get('/backend', 'BackendController@index')->name('backend');
