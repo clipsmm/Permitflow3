4db39cc79d67995056cf42daf71561dac7c8676c
mitajunior <mitacliff@gmail.com>
2017-08-27 22:11:42 +0300

    Minor ui updats

--- resources/lang/en/labels.php
+++ resources/lang/en/labels.php
@@ -5,6 +5,13 @@ return [
 
     'my_applications' => 'My Applications',
     'my_payments' => "My Payments",
-    'downloads' => 'Downloads'
+    'downloads' => 'Downloads',
+    'outputs' => 'Outputs',
+    'output_name_field' => "Name",
+    'output_code_field' => "Unique Code",
+    "output_template_field" => "Template",
+    "cancel" => "Cancel",
+    'save' => "Save",
+    'landing_page' => "Landing Page"
 
 ];
