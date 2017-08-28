6d80aa110a20e1262641bb6ffe150d840e71559d
DESKTOP-7FKJIS9\sonfo <sonfordonyango@gmail.com>
2017-08-28 16:16:29 +0300

    latest updates

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -4,11 +4,11 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-10 col-md-10 col-lg-10 col-xs-12">
-                <div class="panel with-nav-tabs panel-default">
+                <div class="panel  panel-default">
                     <div class="panel-heading p-t-10">
-                        @include('backend.settings._backend_settings_menu')
+                        <h3>@include('backend.settings._backend_settings_menu')</h3>
                     </div>
-                    <div class="panel-body padding-0">
+                    <div class="panel-body">
                         <div class="tab-content">
                             <div class="tab-pane fade in active" id="tab1default">
                                 <div class="panel panel-form m-r-10 m-l-10 m-t-0 m-b-0">
