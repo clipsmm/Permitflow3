c3dea764041e05d2ee5dcc0ff4603f4d8420222d
DESKTOP-7FKJIS9\sonfo <sonfordonyango@gmail.com>
2017-08-28 19:16:23 +0300

    fixed ouputs and login pages

--- resources/views/backend/outputs/new.blade.php
+++ resources/views/backend/outputs/new.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.module')
 @section('body')
-    <div class="panel with-nav-tabs panel-default">
-        <div class="panel-body padding-0">
-            <div class="tab-content">
-                <div class="panel panel-form m-r-10 m-l-10 m-t-0 m-b-0">
-                    <div class="panel-body p-t-10 p-b-10 p-l-0 p-r-0">
-                        <form method="post">
-                            {!! csrf_field() !!}
+    <div class="panel panel-default">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title"> Create Output</h3>
+                    </div>
+                    <form method="post">
+                        {!! csrf_field() !!}
+                    <div class="panel-body ">
+
                             @include('backend.outputs._output_form')
-                            <div class="modal-footer">
-                                <a href="{{ route("backend.outputs.index", $current_module->slug) }}" class="btn btn-default">@lang('labels.cancel')</a>
-                                <button type="submit" class="btn btn-primary">@lang('labels.save')</button>
-                            </div>
-                        </form>
+                    </div>
+                    <div class="panel-footer clearfix">
+                        <div class="pull-right">
+                            <a href="{{ route("backend.outputs.index", $current_module->slug) }}" class="btn btn-default">@lang('labels.cancel')</a>
+                            <button type="submit" class="btn btn-primary">@lang('labels.save')</button>
+                        </div>
 
                     </div>
+                    </form>
                 </div>
-            </div>
-        </div>
     </div>
 @endsection
