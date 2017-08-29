0d63088c85d358b5099ba0044af1c01f6fcd0077
Steve Ops <steve.opiyo5@gmail.com>
2017-08-29 14:35:56 +0300

    Rename link from Personal to Dashboard

--- resources/views/layouts/partials/topnave2.blade.php
+++ resources/views/layouts/partials/topnave2.blade.php
@@ -3,19 +3,25 @@
         <div class="navbar-header">
             <div class="col-sm-2 col-md-2 col-lg-2 hidden-xs">
                 <a class="navbar-brand" href="{{ route('frontend') }}">
-                    <span class="hidden-xs">Personal</span>
+                    <span class="hidden-xs">
+                        @lang('Dashboard')
+                    </span>
                     <span  class=""></span>
                 </a>
             </div>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav tp-icon">
-                <li><a href="#"><strong>Make Application</strong></a></li>
+                <li><a href="#"><strong>
+                            @lang('Make Application')
+                        </strong></a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right hidden-xs">
                 @if(user())
                     <li class="#">
-                        <a href="{{ route('backend.settings.general') }}"><i class="fa fa-cogs"></i> Settings</a>
+                        <a href="{{ route('backend.settings.general') }}"><i class="fa fa-cogs"></i>
+                            @lang('Settings')
+                        </a>
                     </li>
                 @endif
             </ul>
