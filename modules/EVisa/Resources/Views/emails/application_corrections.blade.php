7ff19c4732523db57e69daf69ff1f3218ef0e06b
mitajunior <mitacliff@gmail.com>
2017-09-05 00:26:35 +0300

    - Resolved issue #39: Correction email notification should have a link to edit the application.

--- modules/EVisa/Resources/Views/emails/application_corrections.blade.php
+++ modules/EVisa/Resources/Views/emails/application_corrections.blade.php
@@ -1,1 +1,1 @@
-Hi, your application {{ $application->application_number }} requires corrections. Please login and make the corrections
+Hi, your application {{ $application->application_number }} requires corrections. <a href="{{$application->module->get_edit_url($application)}}">Click here</a> to edit your application and resubmit.
