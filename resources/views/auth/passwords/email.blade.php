97c47b043069eac7fde389c85d8684ff552fe9e8
DESKTOP-7FKJIS9\sonfo <sonfordonyango@gmail.com>
2017-09-05 16:27:05 +0300

    fixed requirements page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
+    <header>
+        <nav class="navbar navbar-inverse    navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'eVisa') }}
+                    </a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            @include('layouts.partials.topnave2')
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Profile</a></li>
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                              style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <nav class="navbar navbar-default navbar-fixed-top"
+             style="margin-top: 50px; background-color: #FFF; z-index: 900;">
+            <div class="container">
+                <div class="navbar-header">
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav tp-icon">
+                        <li><a href="#"><strong>Overview</strong></a></li>
+                        <li><a href="{{ route('eligibility') }}">Eligibility</a></li>
+                        <li><a href="{{ route('faq') }}">FAQs</a></li>
+                        <li><a href="contacts.html">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -29,7 +85,7 @@
                                 @endif
                             </div>
                         </div>
-
+                       <br/>
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
