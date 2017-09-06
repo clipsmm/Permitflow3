ae33e4df8dfa0f26b6635b65b109b61a92e95897
DESKTOP-7FKJIS9\sonfo <sonfordonyango@gmail.com>
2017-09-07 02:51:38 +0300

    tasks

--- resources/views/layouts/module.blade.php
+++ resources/views/layouts/module.blade.php
@@ -9,42 +9,13 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h3 class="task-icon2">
-                            <span class="task-img2"><img src="images/task4.svg"/></span>
+                            <span class="task-img2"><img src="{{ asset('resources/assets/images/task4.svg') }}"/></span>
                             <span class="task-img2">Tasks</span>
                         </h3>
                         <div class="pull-right" role="">
                             @foreach($current_module->get_menus() as $menu)
                                 <a class="btn btn-sm btn-{{ array_get($menu, 'type') }}" href="{{ array_get($menu, 'action') }}"><i class="{{ array_get($menu, 'icon') }}"></i> {{ array_get($menu,'name') }}</a>
                             @endforeach
-
-                                {{--<div class="panel-heading clearfix">--}}
-
-                                    {{--<h3 class="task-icon2">--}}
-                                        {{--<span class="task-img2"><img src="images/task4.svg"/></span>--}}
-                                        {{--<span class="task-img2">Tasks</span>--}}
-                                    {{--</h3>--}}
-                                    {{--<div class="pull-right" role="">--}}
-
-                                        {{--<a href="#" class="task-icon">--}}
-                                            {{--<span class="task-img"><img src="images/task4.svg"/></span>--}}
-                                            {{--<span class="task-img">Tasks</span>--}}
-                                        {{--</a>--}}
-                                        {{--<a href="#" class="task-icon">--}}
-                                            {{--<span class="task-img"><img src="images/tasks.svg"/></span>--}}
-                                            {{--<span class="task-img">Applications</span>--}}
-                                        {{--</a>--}}
-                                        {{--<a href="#" class="task-icon">--}}
-                                            {{--<span class="task-img"><img src="images/reports.svg"/></span>--}}
-                                            {{--<span class="task-img">Reports</span>--}}
-                                        {{--</a>--}}
-                                        {{--<a href="#" class="task-icon">--}}
-                                            {{--<span class="task-img"><img src="images/set4.svg"/></span>--}}
-                                            {{--<span class="task-img">Settings</span>--}}
-                                        {{--</a>--}}
-                                    {{--</div>--}}
-
-
-                                {{--</div>--}}
                         </div>
                         <div class="clearfix"></div>
                     </div>
