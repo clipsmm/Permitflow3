ec2cc54179be1ca1feec3dc25e2bb3e526d5682d
mitajunior <mitacliff@gmail.com>
2017-12-05 22:51:38 +0300

    Added site general info to settings

--- resources/views/layouts/partials/topnav1.blade.php
+++ resources/views/layouts/partials/topnav1.blade.php
@@ -10,7 +10,7 @@
             </button>
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'eVisa') }}
+                {{ settings('site_name', 'eVisa') }}
             </a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
