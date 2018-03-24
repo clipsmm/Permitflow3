461990ead5fcc599be5fd3fdb34dc8445f1cd36e
Steve Ops <steve.opiyo5@gmail.com>
2018-03-24 15:27:29 +0300

    Smaller registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,21 +10,16 @@
     @endif
     <div id="vue-root" class="container">
         <div class="row m-t-0">
-            <div class="col-md-12">
-                <div class="panel panel-default panel-form m-r-10 m-l-10">
-                    <div class="panel-body p-t-10 p-b-10 p-l-0 p-r-0">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-
-
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Register</h3>
-                        </div>
-                        <form method="POST" action="{{ route('register') }}">
+            <div class="col-md-8 col-sm-offset-2">
+                <div class="panel panel-default panel-form">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Register</h3>
+                    </div>
+                    <form method="POST" action="{{ route('register') }}">
 
                         <div class="panel-body">
                             {{ csrf_field() }}
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, 'first_name') }}">
                                     <label for="name" class="control-label">@lang("First Name")</label>
                                     <input id="name" type="text" class="form-control" name="first_name"
@@ -33,7 +28,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, 'surname') }}">
                                     <label for="surname" class="control-label">@lang("Surname")</label>
                                     <input id="surname" type="text" class="form-control" name="surname"
@@ -42,7 +37,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, "last_name") }}">
                                     <label for="last_name" class="control-label">@lang("Last Name")</label>
                                     <input id="last_name" type="text" class="form-control" name="last_name"
@@ -51,7 +46,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, "email") }}">
                                     <label for="email" class="control-label">@lang("Email")</label>
                                     <input id="email" type="email" class="form-control" name="email"
@@ -60,7 +55,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, "phone_number") }}">
                                     <label for="phone_number" class="control-label">@lang("Phone Number")</label>
                                     <intl-telephone-input value="{{old('phone_number')}}" name="phone_number" ></intl-telephone-input>
@@ -68,7 +63,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, "gender") }}">
                                     <label for="gender" class="control-label">@lang("Gender")</label>
                                     {{Form::select("gender", ["M" => __("Male"), "F" => __("Female"), "other" => __("Other")], "M", ['class' => 'form-control']) }}
@@ -76,7 +71,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, "id_number") }}">
                                     <label for="id_number" class="control-label">@lang("Id Number")</label>
                                     <input id="id_number" type="text" class="form-control" name="id_number"
@@ -85,7 +80,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, "id_type") }}">
                                     <label for="id_type" class="control-label">@lang("Id Type")</label>
                                     {{Form::select("id_type", ["citizen" => __("Citizen"), "alien" => __("Foreign Resident"), "visitor" => __("Foreigner")], "citizen", ['class' => 'form-control']) }}
@@ -93,7 +88,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, "password") }}">
                                     <label for="password" class="control-label">@lang("Password")</label>
                                     {{Form::password("password", ['class' => 'form-control']) }}
@@ -101,33 +96,29 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group{{ error_class($errors, "password_confirmation") }}">
                                     <label for="password_confirmation"
                                            class="control-label">@lang("Password Confirmation")</label>
                                     {{Form::password("password_confirmation", ['class' => 'form-control']) }}
                                     {!! error_tag($errors, "password_confirmation") !!}
-                                 </div>
+                                </div>
                             </div>
                         </div>
 
                         <div class="panel-footer text-right">
-                                <span class="pull-left">Already have an account? <a href="{{ url('login') }}">Sign In</a>  </span>
+                            <span class="pull-left">Already have an account? <a href="{{ url('login') }}">Sign In</a>  </span>
 
-                                <a href="{{config('app.ss-on-url')}}" type="submit" class="btn btn-default">
-                                    @lang('Register via eCitizen')
-                                </a>
+                            <a href="{{config('app.ss-on-url')}}" type="submit" class="btn btn-default">
+                                @lang('Register via eCitizen')
+                            </a>
 
                             <button type="submit" class="btn btn-primary">
                                 @lang("Register")
                             </button>
                         </div>
-                         </form>
-                     </div>
-                        </div>
-
-                    </div>
-                 </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
